4bc7075d0d1c3960a37cca5fbd7ecf23930a316a
shivadasari88 <dasarishivasai71@gmail.com>
2025-12-07 10:18:13 +0530

    connected and manipulated the database

--- DemoHib/src/main/java/com/shiva/DemoHib/App.java
+++ DemoHib/src/main/java/com/shiva/DemoHib/App.java
@@ -1,13 +1,30 @@
 package com.shiva.DemoHib;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Person shiva = new Person();
+    	shiva.setPid(101);
+    	shiva.setPname("shiva");
+    	shiva.setColor("white");
+    	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(Person.class);
+    	SessionFactory sf = con.buildSessionFactory();
+    	Session session = sf.openSession();
+    	
+    	Transaction tx = session.beginTransaction();
+    			
+    	session.persist(shiva);
+    	
+    	tx.commit();
+    	
     }
 }
+ 
--- DemoHib/src/main/java/com/shiva/DemoHib/Person.java
+++ DemoHib/src/main/java/com/shiva/DemoHib/Person.java
@@ -1,5 +1,30 @@
 package com.shiva.DemoHib;
 
-public class Person {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class Person {
+	@Id
+	private int pid;
+	private String pname;
+	private String color;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
